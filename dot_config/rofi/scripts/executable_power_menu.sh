996cd3fb6f0c3feecb41d1fac6f79e3879ea949f
PascalH214 <pascal02012004@freenet.de>
2026-02-11 11:41:56 +0100

    Add some power-management

--- dot_config/rofi/scripts/executable_power_menu.sh
+++ dot_config/rofi/scripts/executable_power_menu.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-if [ x"$@" = x"lock" ]; then
+if [ x"$@" = x"hibernate" ]; then
+  hyprlock &
+  sleep 1
+  systemctl hibernate
+  exit 0
+elif [ x"$@" = x"suspend" ]; then
+  hyprlock &
+  sleep 1
+  systemctl suspend
+  exit 0
+elif [ x"$@" = x"lock" ]; then
   hyprlock &
   exit 0
 elif [ x"$@" = x"logout" ]; then
@@ -20,3 +30,5 @@ echo -en "shutdown\0icon\x1f./icons/shutdown.svg\n"
 echo -en "reboot\0icon\x1f./icons/reboot.svg\n"
 echo -en "logout\0icon\x1f./icons/logout.svg\n"
 echo -en "lock\0icon\x1f./icons/lock.svg\n"
+echo -en "suspend\0icon\x1f./icons/suspend.svg\n"
+echo -en "hibernate\0icon\x1f./icons/hibernate.svg\n"
