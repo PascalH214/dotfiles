e66a2fe8b9bd3e596dec1bca30787736be7a8f21
PascalH214 <pascal02012004@freenet.de>
2026-02-17 20:07:15 +0100

    Add some scripts to enforce window-rules

--- dot_config/hypr/scripts/executable_main-watchers.sh
+++ dot_config/hypr/scripts/executable_main-watchers.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 while true; do
-  "$HOME/.config/hypr/scripts/enforce-workspace9.sh"
-  "$HOME/.config/hypr/scripts/enforce-overwatch-fullscreen.sh"
+  "$HOME/.config/hypr/scripts/enforcer/enforce-workspace9.sh"
+  "$HOME/.config/hypr/scripts/enforcer/enforce-workspace8.sh"
+  "$HOME/.config/hypr/scripts/enforcer/enforce-overwatch-fullscreen.sh"
 
   sleep 0.5
 done
