6ad2c4d36372ffaebbc23a9674ba10403c0353b5
PascalH214 <pascal02012004@freenet.de>
2026-02-18 13:36:01 +0100

    Make changes to bar

--- dot_config/ags/widget/Bar.tsx
+++ dot_config/ags/widget/Bar.tsx
@@ -3,19 +3,35 @@ import { Astal, Gtk, Gdk } from "ags/gtk4"
 
 import Hyprland from "gi://AstalHyprland"
 import { createBinding, createEffect, createState, For } from "gnim"
+import GObject from "gnim/gobject";
 
 const hyprland = Hyprland.get_default();
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
   const workspacesProperty = createBinding(hyprland, "workspaces");
+  const focusedWorkspaceId = createBinding(hyprland, "focused_workspace", "id")
 
-  const [workspaces, setWorkspaces] = createState<Hyprland.Workspace[]>([]);
+  const [buttons, setButtons] = createState<GObject.Object[]>([]);
   
   createEffect(() => {
-    let sorted_workspaces = workspacesProperty();
+    const sorted_workspaces = workspacesProperty().slice();
     sorted_workspaces.sort((a, b) => a.get_id() - b.get_id());
-    setWorkspaces(sorted_workspaces);
+
+    const this_buttons = sorted_workspaces.map(workspace => {
+      const id = workspace.get_id();
+      const idStr = id.toString();
+      
+      return (
+        <button 
+          class={id == focusedWorkspaceId() ? "focused" : ""}
+          label={idStr == "10" ? "0" : idStr}
+          onClicked={() => workspace.focus()}
+        />
+      )
+    });
+
+    setButtons(this_buttons);
   })
 
   return (
@@ -30,15 +46,12 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
     >
       <box
         class="workspaces"
-        halign={Gtk.Align.START}
+        halign={Gtk.Align.CENTER}
         hexpand={false}
       >
-        <For each={workspaces}>
-          {(item, index) => {
-            const id = item.get_id().toString();
-
-            return <button label={id == "10" ? "0" : id}/>
-          }}
+        <For each={buttons}>
+          {(item, index) => (item)
+          }
         </For>
       </box>
     </window>
