73b6586eaeaeb9695929ae25fe615007550d582f
PascalH214 <pascal02012004@freenet.de>
2026-03-01 19:10:31 +0100

    Add enforcer for special-workspace

--- dot_config/hypr/scripts/enforcer/executable_enforce-workspace8.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-exec "$HOME/.config/hypr/scripts/enforcer/enforce-workspace-by-class.sh" "8" "discord" "feishin"
--- dot_config/hypr/scripts/executable_main-watchers.sh
+++ dot_config/hypr/scripts/executable_main-watchers.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 while true; do
+  "$HOME/.config/hypr/scripts/enforcer/enforce-special.sh"
   "$HOME/.config/hypr/scripts/enforcer/enforce-workspace9.sh"
-  "$HOME/.config/hypr/scripts/enforcer/enforce-workspace8.sh"
   "$HOME/.config/hypr/scripts/enforcer/enforce-overwatch-fullscreen.sh"
 
   sleep 0.5
